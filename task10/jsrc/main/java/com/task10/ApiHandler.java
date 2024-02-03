2865e08083a31eef58583bc0d60d57363571740c
m-sobur <msobur123@gmail.com>
2024-02-03 19:52:02 +0200

    Revert "Task 10 changed registerUserInCognito via   List<AttributeType> userAttrs"
    This reverts commit 653b3ef5bacb605a025610fbf9cfeaddf55b2b30.

--- task10/jsrc/main/java/com/task10/ApiHandler.java
+++ task10/jsrc/main/java/com/task10/ApiHandler.java
@@ -32,9 +32,7 @@ import software.amazon.awssdk.services.cognitoidentityprovider.model.ListUserPoo
 import software.amazon.awssdk.services.cognitoidentityprovider.model.SignUpRequest;
 import software.amazon.awssdk.services.cognitoidentityprovider.model.UserPoolDescriptionType;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @LambdaHandler(lambdaName = "api_handler",
@@ -197,10 +195,10 @@ public class ApiHandler implements RequestHandler<APIGatewayProxyRequestEvent, O
 
 
     private AdminConfirmSignUpResponse registerUserInCognito(String email, String password, String firstName, String lastName) {
-        List<AttributeType> userAttrs = Arrays.asList(
-                AttributeType.builder().name("name").value(firstName + " " + lastName).build(),
-                AttributeType.builder().name("email").value(email).build()
-        );
+        AttributeType userAttrs = AttributeType.builder()
+                .name("name").value(firstName + " " + lastName)
+                .name("email").value(email)
+                .build();
         SignUpRequest signUpRequest = SignUpRequest.builder()
                 .userAttributes(userAttrs)
                 .username(email)
